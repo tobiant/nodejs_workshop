b13d1a3f1a835c9874965e9b8a63f309a1af552d
Leon Möller <lmr@inwerken.de>
2019-06-12 12:49:11 +0200

    update

--- app.js
+++ app.js
@@ -12,34 +12,38 @@ app.use(express.json())
 
 // helloworld Get Request
 app.get('/helloworld', (req, res) => {
-    res.json({'hello':'world'})
+    res.json({
+        'hello': 'world'
+    })
 })
 
 // recipes Get Request
 app.get('/recipes', (req, res) => {
-    // variable with valid sql query
-    let query = 'SELECT * FROM recipes;'
-    // use variable for query
-    db.all(query, [], (err, rows) => {
-        // In case the query fails
-        if (err) {
-            res.json(err)
-        // in case the query works good
-        } else {
-            res.json(rows)
-        }
-    })
+    db.all("SELECT * FROM recipes",
+        [],
+        (err, rows) => {
+            // In case the query fails
+            if (err) {
+                res.json(err)
+                // in case the query works good
+            } else {
+                res.json(rows)
+            }
+        })
 })
 
 // recipes post request
 app.post('/recipes', (req, res) => {
     // preparing the query (?=wildcard, will be filled with req.body in case the request is succesful)
-    const query = db.prepare('INSERT INTO recipes(name, description, minutes_needed) VALUES(?,?,?)')
-    query.run(req.body.name, req.body.description, req.body.minutes_needed, (err, rows) => {
+    db.run("INSERT INTO recipes(name, description, minutes_needed) VALUES($name, $description, $minutes_needed)", {
+        $name: req.body.name,
+        $description: req.body.description,
+        $minutes_needed: req.body.minutes_needed
+    }, (err, rows) => {
         if (err) {
             res.json(err)
         } else {
-            db.get('SELECT * FROM recipes WHERE id = (SELECT MAX(id) FROM recipes)', (err, row) => {
+            db.get("SELECT * FROM recipes WHERE id = (SELECT MAX(id) FROM recipes)", (err, row) => {
                 if (err) {
                     res.json(err)
                 } else {
@@ -50,17 +54,42 @@ app.post('/recipes', (req, res) => {
     })
 })
 
-app.delete('/recipes/:id', (req, res) => {
-    const query = db.prepare('DELETE FROM recipes WHERE id = ?')
-    query.run(req.params.id, (err, rows) => {
+app.delete('/recipes', (req, res) => {
+    // const query = db.prepare('DELETE FROM recipes WHERE id = ?')
+    db.run("DELETE FROM recipes WHERE id = $id", {
+        $id: req.query.id
+    }, (err, rows) => {
         if (err) {
             res.json(err)
         } else {
-            res.json('Item with id' + req.params.id + 'succesfull deleted!')
+            res.json('Item with id ' + req.query.id + ' succesfully deleted!')
         }
     })
 })
 
-app.put('/recipes')
+app.put('/recipes', (req, res) => {
+    db.run("UPDATE recipes SET name = $name, description = $description, minutes_needed = $minutes_needed WHERE id = $id ;", {
+            $name: req.body.name,
+            $description: req.body.description,
+            $minutes_needed: req.body.minutes_needed,
+            $id: req.body.id
+        },
+        (err, rows) => {
+            if (err) {
+                res.json(err)
+            } else {
+                db.get("SELECT * FROM recipes WHERE id = $id", {
+                    $id: req.body.id
+                }, (err, rows) => {
+                    if (err) {
+                        res.json(err)
+                    } else {
+                        res.json(rows)
+                    }
+                })
+            }
+        }
+    )
+})
 // Starting the server
-app.listen(port, ()=> console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
