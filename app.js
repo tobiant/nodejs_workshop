66bafcbf579d6ec1443a69f27f3ba601c951b8fd
Tobias <tge@inwerken.de>
2019-06-19 11:04:17 +0200

    crud

--- app.js
+++ app.js
@@ -11,11 +11,60 @@ const db = new sqlite3.Database('recipes.sqlite')
 app.use(express.json())
 
 // helloworld Get Request
-app.get('/helloworld', (req, res) => {
-    res.json({
-        'hello': 'world'
+app.get('/recipes', (req, res) => {
+    db.all("SELECT * FROM recipes",
+    (err, rows) => {
+        if(err){
+            res.json(err)
+        } else {
+            res.json(rows)
+        }
     })
 })
+
+app.post("/recipes", (req, res) => {
+    db.all("INSERT INTO recipes(name, description, minutes_needed) VALUES($name, $description, $minutes_needed)", {
+        $name: req.body.name,
+        $description: req.body.description,
+        $minutes_needed: req.body.minutes_needed,
+    }, (err, rows) => {
+        if(err) {
+            res.status(400).json(err) 
+        } else {
+            res.status(201).json(rows)
+        }
+    })
+})
+
+app.put("/recipes", (req, res) => {
+    db.all("UPDATE recipes SET name = $name, description = $description, minutes_needed = $minutes_needed WHERE id = $id", {
+        $id: req.body.id,
+        $name: req.body.name,
+        $description: req.body.description,
+        $minutes_needed: req.body.minutes_needed,
+    }, (err, rows) => {
+        if(err) {
+            res.status(400).json(err) 
+        } else {
+            res.status(201).json(rows)
+        }
+    })
+})
+
+app.delete("/recipes", (req, res) => {
+    db.all("DELETE FROM recipes WHERE id = $id", {
+        $id: req.body.id
+    }, (err, rows) => {
+        if(err) {
+            res.status(400).json(err) 
+        } else {
+            res.status(201).json(rows)
+        }
+    })
+})
+
+
+
 
 
 // Starting the server
